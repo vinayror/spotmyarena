656a8accdc153968b7688cbc3fb54dde08c85af6
vinayror <vnysharma22@gmail.com>
2015-09-08 00:13:31 +0530

    made some minor changes for auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   #config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :facebook, "1144085172288188", "5db1fb7a8c01609ceb740471a621fbc9" , scope: 'email', info_fields: 'email, first_name, last_name',  :strategy_class => OmniAuth::Strategies::Facebook,  :display => 'popup', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  config.omniauth :facebook, "1144085172288188", "5db1fb7a8c01609ceb740471a621fbc9" , scope: 'email', info_fields: 'email, first_name, last_name',  :strategy_class => OmniAuth::Strategies::Facebook,  :display => 'popup', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
   
   config.omniauth :google_oauth2, "718604841377-b577j78thfsbur146ijgg9clb8cnu2vu.apps.googleusercontent.com", "2Ee4i-MfA3tfiaMTycWvUWLk", { access_type: "offline", approval_prompt: "" }
   # Configure the class responsible to send e-mails.
