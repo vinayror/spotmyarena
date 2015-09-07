8d2f6cc4ed671587d6230e87d93680b53b424ce9
vinayror <vnysharma22@gmail.com>
2015-09-08 01:32:26 +0530

    made some minor changes for auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
     
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable#, :confirmable
 
     def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
       user = User.where(:provider => auth.provider, :uid => auth.uid).first
