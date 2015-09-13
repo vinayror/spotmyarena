8c26864988602832c9386b845218e052179190a3
vinayror <vnysharma22@gmail.com>
2015-09-14 02:07:06 +0530

    Made some changes

--- app/models/booking_date.rb
+++ app/models/booking_date.rb
@@ -1,6 +1,6 @@
 class BookingDate < ActiveRecord::Base
 	belongs_to :ground
-	has_many :booking_times
+	has_many :booking_times, :dependent => :destroy
 	accepts_nested_attributes_for :booking_times, reject_if: :all_blank, allow_destroy: true
 
 	validates_uniqueness_of :date_of_booking
