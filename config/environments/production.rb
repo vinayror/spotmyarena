24d3fb98c36c6a30670d7a0ac8f80b710f6820df
Ram Laxman Yadav <ram@headerlabs.com>
2015-10-07 03:46:33 +0530

    running test mode for payment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,9 +88,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile += %w( assets/stylesheets/square)
-
-
   config.action_mailer.default :charset => "utf-8"
 
 
