651572fc3ea7c574210619cc3b9721801903d740
Ram Laxman Yadav <ram@headerlabs.com>
2015-10-08 23:17:58 +0530

    aading rails admin css and js for compile in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,10 +89,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default :charset => "utf-8"
-  config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
 
-
-  config.action_mailer.default_url_options = {:host => 'http://spotmyarena.com'}
+  config.action_mailer.default_url_options = {:host => '188.166.250.248'}
 
   config.action_mailer.smtp_settings = {
       :authentication => :plain,
